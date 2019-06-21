54d99eea4f1b92953df48a2bb199bffd91be3c76
DevArgoz <51326324+DevArgoz@users.noreply.github.com>
2019-06-21 14:10:03 +0200

    Update source.lua

--- docs/source.lua
+++ docs/source.lua
@@ -3,7 +3,7 @@ getgenv().char = function()
 end
 getgenv().playergui = function()
 	return game.Players.LocalPlayer.PlayerGui
-end 
+end
 getgenv().playerscripts = function()
 	return game.Players.LocalPlayer.PlayerScripts
 end
@@ -13,12 +13,14 @@ end
 getgenv().tp = function(cfr)
 	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = cfr
 end
-getgenv().create = function(class,props)
+getgenv().create = function(class, props)
 	local inst = Instance.new(class)
-	for i,v in pairs(props) do 
-		pcall(function()
-			inst[i] = v
-		end)
+	for i, v in pairs(props) do
+		pcall(
+			function()
+				inst[i] = v
+			end
+		)
 	end
 	return inst
 end
@@ -35,14 +37,79 @@ end
 getgenv().setgravity = function(val)
 	workspace.Gravity = val
 end
+getgenv().join_place = function(placeId)
+	game:GetService("TeleportService"):Teleport(placeId, game.Players.LocalPlayer)
+end
+getgenv().setupvalue = function(_script, upvalue, val)
+	for i, v in pairs(debug.getregistry()) do
+		if type(v) == "function" and getfenv(v).script == _script then
+			if debug.getupvalue(v, upvalue) then
+				debug.setupvalue(v, upvalue, val)
+				return true
+			end
+		end
+	end
+	return false
+end
+getgenv().getupvalue = function(_script, upvalue)
+	for i, v in pairs(debug.getregistry()) do
+		if type(v) == "function" and getfenv(v).script == _script then
+			if debug.getupvalue(v, upvalue) ~= nil then
+				return debug.getupvalue(v, upvalue)
+			end
+		end
+	end
+	return nil
+end
 getgenv().script_db = {
 	SPR = {
-		Kill_All = function() loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/DevArgoz/Roblox-Enviorment-Extension/master/docs/SPR/Kill_All.lua",true))() end,
-		Taze_All = function() loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/DevArgoz/Roblox-Environment-Extension/master/docs/SPR/SPR%20Taze%20all.lua",true))() end,
-		OP_Weapons = function() loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/DevArgoz/Roblox-Environment-Extension/master/docs/SPR/OP_Weapons.lua",true))() end,
-		Bring_All = function() loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/DevArgoz/Roblox-Environment-Extension/master/docs/SPR/Bring%20all%20players.lua",true))() end
+		Kill_All = function()
+			loadstring(
+				game:HttpGetAsync(
+					"https://raw.githubusercontent.com/DevArgoz/Roblox-Enviorment-Extension/master/docs/SPR/Kill_All.lua",
+					true
+				)
+			)()
+		end,
+		Taze_All = function()
+			loadstring(
+				game:HttpGetAsync(
+					"https://raw.githubusercontent.com/DevArgoz/Roblox-Environment-Extension/master/docs/SPR/SPR%20Taze%20all.lua",
+					true
+				)
+			)()
+		end,
+		OP_Weapons = function()
+			loadstring(
+				game:HttpGetAsync(
+					"https://raw.githubusercontent.com/DevArgoz/Roblox-Environment-Extension/master/docs/SPR/OP_Weapons.lua",
+					true
+				)
+			)()
+		end,
+		Bring_All = function()
+			loadstring(
+				game:HttpGetAsync(
+					"https://raw.githubusercontent.com/DevArgoz/Roblox-Environment-Extension/master/docs/SPR/Bring%20all%20players",
+					true
+				)
+			)()
+		end
 	},
 	RobloxHQ = {
-		GetKicked = function() create("ScreenGui",{Parent=playergui;}) end
+		TriggerAntiExploit = function()
+			create("ScreenGui", {Parent = playergui()})
+			create("Part", {Parent = workspace})
+		end
+	},
+	GhostSim = {
+		AutoFarm = function()
+			loadstring(
+				game:HttpGetAsync(
+					"https://raw.githubusercontent.com/DevArgoz/Roblox-Environment-Extension/master/docs/GhostSim/Autofarm.lua",
+					true
+				)
+			)()
+		end
 	}
 }
