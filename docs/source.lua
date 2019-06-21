d8879da7259ce85e255c2e11b76247f04549ef76
DevArgoz <51326324+DevArgoz@users.noreply.github.com>
2019-06-21 21:15:03 +0200

    Update source.lua

--- docs/source.lua
+++ docs/source.lua
@@ -1,180 +1,157 @@
--- Init REE (Roblox-Environment-Extension)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/DevArgoz/Roblox-Environment-Extension/master/docs/source.lua",true))()
--- Delete old GUI
-if game.CoreGui:FindFirstChild("DevConsole") then game.CoreGui.DevConsole:Destroy() end
-if _G.DevConsoleScript then _G.DevConsoleScript:Destroy() end
-_G.DevConsoleScript = script
-
-local services = {}
-local meta = {
-	__index = function(self,v)
-		return game:GetService(v)
+getgenv().char = function()
+	return game.Players.LocalPlayer.Character
+end
+getgenv().playergui = function()
+	return game.Players.LocalPlayer.PlayerGui
+end
+getgenv().playerscripts = function()
+	return game.Players.LocalPlayer.PlayerScripts
+end
+getgenv().leaderstats = function()
+	return game.Players.LocalPlayer.leaderstats
+end
+getgenv().tp = function(cfr)
+	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = cfr
+end
+getgenv().create = function(class, props)
+	local inst = Instance.new(class)
+	for i, v in pairs(props) do
+		pcall(
+			function()
+				inst[i] = v
+			end
+		)
 	end
+	return inst
+end
+getgenv().sethealth = function(val)
+	game.Players.LocalPlayer.Character.Humanoid.MaxHealth = val
+	game.Players.LocalPlayer.Character.Humanoid.Health = val
+end
+getgenv().setwalkspeed = function(val)
+	game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = val
+end
+getgenv().setjumppower = function(val)
+	game.Players.LocalPlayer.Character.Humanoid.JumpPower = val
+end
+getgenv().setgravity = function(val)
+	workspace.Gravity = val
+end
+getgenv().join_place = function(placeId)
+	game:GetService("TeleportService"):Teleport(placeId, game.Players.LocalPlayer)
+end
+getgenv().setupvalue = function(_script, upvalue, val)
+	for i, v in pairs(debug.getregistry()) do
+		if type(v) == "function" and getfenv(v).script == _script then
+			if debug.getupvalue(v, upvalue) then
+				debug.setupvalue(v, upvalue, val)
+				return true
+			end
+		end
+	end
+	return false
+end
+getgenv().getupvalue = function(_script, upvalue)
+	for i, v in pairs(debug.getregistry()) do
+		if type(v) == "function" and getfenv(v).script == _script then
+			if debug.getupvalue(v, upvalue) ~= nil then
+				return debug.getupvalue(v, upvalue)
+			end
+		end
+	end
+	return nil
+end
+getgenv().instanceByPath = function(path)
+	local x = {}
+	local last = ""
+	for i=1,#path do
+		local y = string.sub(path,i,i)
+		if y == "." then
+			table.insert(x,#x+1,last)
+			last = ""
+		else
+			last = last .. y
+		end
+	end
+	table.insert(x,#x+1,last)
+	for i=1,#x do 
+		if x[i] == "LocalPlayer" then 
+			x[i] = game.Players.LocalPlayer.Name
+		end
+	end
+	for i,v in pairs(game:GetDescendants()) do 
+		if v.Name == x[#x] then 
+			if v:GetFullName() == table.concat(x,".") then 
+				return v
+			end
+		end
+	end
+	return nil
+end
+getgenv().splitString = function(text,ind)
+	local args = {}
+	local last = ""
+	for i=1,#text do
+		local x = string.sub(text,i,i)
+		if x == ind  then
+			table.insert(args,#args+1,last)
+			last = ""
+		else
+			last = last .. x
+		end
+	end
+	table.insert(args,#args+1,last)
+	return args
+end
+getgenv().script_db = {
+	SPR = {
+		Kill_All = function()
+			loadstring(
+				game:HttpGetAsync(
+					"https://raw.githubusercontent.com/DevArgoz/Roblox-Enviorment-Extension/master/docs/SPR/Kill_All.lua",
+					true
+				)
+			)()
+		end,
+		Taze_All = function()
+			loadstring(
+				game:HttpGetAsync(
+					"https://raw.githubusercontent.com/DevArgoz/Roblox-Environment-Extension/master/docs/SPR/SPR%20Taze%20all.lua",
+					true
+				)
+			)()
+		end,
+		OP_Weapons = function()
+			loadstring(
+				game:HttpGetAsync(
+					"https://raw.githubusercontent.com/DevArgoz/Roblox-Environment-Extension/master/docs/SPR/OP_Weapons.lua",
+					true
+				)
+			)()
+		end,
+		Bring_All = function()
+			loadstring(
+				game:HttpGetAsync(
+					"https://raw.githubusercontent.com/DevArgoz/Roblox-Environment-Extension/master/docs/SPR/Bring%20all%20players",
+					true
+				)
+			)()
+		end
+	},
+	RobloxHQ = {
+		TriggerAntiExploit = function()
+			create("ScreenGui", {Parent = playergui()})
+			create("Part", {Parent = workspace})
+		end
+	},
+	GhostSim = {
+		AutoFarm = function()
+			loadstring(
+				game:HttpGetAsync(
+					"https://raw.githubusercontent.com/DevArgoz/Roblox-Environment-Extension/master/docs/GhostSim/Autofarm.lua",
+					true
+				)
+			)()
+		end
+	}
 }
-debug.setmetatable(services,meta)
-
-local cmds = {}
-local shortPaths = {}
-local focused = false
-local debounce = false
-
-local gui = create("ScreenGui",{Parent=game.CoreGui;Name="DevConsole";})
-local mainFrame = create("Frame",{Parent=gui;Size=UDim2.new(1,0,.3,0);BackgroundColor3=Color3.fromRGB(40,40,40);ZIndex=9e9;ClipsDescendants=true})
-local logBox = create("Frame",{Parent=mainFrame;Size=UDim2.new(1,0,.95,0);BackgroundColor3=Color3.fromRGB(50,50,50);ClipsDescendants=true})
-local inputBox = create("TextBox",{Parent=mainFrame;Size=UDim2.new(1,0,.05,0);Position=UDim2.new(0,0,.95,0);TextXAlignment=Enum.TextXAlignment.Left;TextSize=12;BackgroundTransparency=1;TextColor3=Color3.fromRGB(255,255,255);Text="";ClearTextOnFocus=false;})
-
--- Extra GUI Instances
-local UIList = create("UIListLayout",{Parent=logBox;Padding=UDim.new(0,3);SortOrder=Enum.SortOrder.LayoutOrder;VerticalAlignment=Enum.VerticalAlignment.Bottom;})
-
-local function SendLog(text,color)
-	local num = (#logBox:GetChildren() - 1) + 1
-	local tl = create("TextLabel",{Parent=logBox;Text=text;TextColor3=color;TextScaled=true;Font=Enum.Font.Gotham;Name=num;TextXAlignment=Enum.TextXAlignment.Left;Size=UDim2.new(1,0,0,17);BackgroundTransparency=1})
-end
-
-local function UpdateLastLog(text,color)
-	local obj = logBox:FindFirstChild(tostring((#logBox:GetChildren() - 1)))
-	obj.Text = text
-	obj.TextColor3 = color
-end
-
-function cmds.getupval(path,upval)
-	debounce = true
-	if string.lower(path:sub(1,5)) == "game." then path = path:sub(6,#path) end
-	if shortPaths[path] then
-		path = shortPaths[path]
-	end
-	local upvalue = getupvalue(instanceByPath(path),upval) or "NO VALUE FOUND!"
-	SendLog("[" .. path .. "] local " .. upval .. " equals to " .. tostring(upvalue),Color3.fromRGB(255,255,0))
-	debounce = false
-end
-
-function cmds.setupval(path,upval,val)
-	debounce = true
-	if string.lower(path:sub(1,5)) == "game." then path = path:sub(6,#path) end
-	if shortPaths[path] then
-		path = shortPaths[path]
-	end
-	setupvalue(instanceByPath(path),upval,val)
-	SendLog("[" .. path .. "] Changed local " .. upval .. " to " .. val,Color3.fromRGB(255,255,0))
-	debounce = false
-end
-
-function cmds.copy_reg(...)
-	if not syn then SendLog("This command is Synapse X only.",Color3.fromRGB(200,0,0)) return end
-	debounce = true
-	local reg = debug.getregistry()
-	local str = ""
-	SendLog("Copieing " .. #reg .. " objects...",Color3.fromRGB(255,255,0))
-	wait(.2)
-	for i,v in pairs(reg) do 
-		str = str .. tostring(i) .. " => " .. tostring(v) .. "\n"
-	end
-	syn.write_clipboard(str)
-	SendLog("Copied registry to clipboard.",Color3.fromRGB(0,180,0))
-	debounce = false
-end
-
-function cmds.cls(...)
-	debounce = true
-	for i,v in pairs(UIList.Parent:GetChildren()) do 
-		if not v:IsA("UIListLayout") then 
-			v:Destroy()
-		end
-	end
-	debounce = false
-end
-
-function cmds.cmds()
-	debounce = true
-	SendLog("Commands:",Color3.fromRGB(255,255,255))
-	SendLog(" cls - Clears the console",Color3.fromRGB(255,255,255))
-	SendLog(" getupval <PATH> <UPVALUE> - Returns the given upvalue of the given script",Color3.fromRGB(255,255,255))
-	SendLog(" setupval <PATH> <UPVALUE> <VALUE> - Sets the given upvalue of the given script",Color3.fromRGB(255,255,255))
-	SendLog(" copy_reg - Copies the roblox registry to your clipboard",Color3.fromRGB(255,255,255))
-	SendLog(" exec <Code/Source> - Executes the given code/source.",Color3.fromRGB(255,255,255))
-	SendLog(" path <new/del> <Name> <Path> - Saves/Deletes a path.",Color3.fromRGB(255,255,255))
-	SendLog(" copy_path <Path> - Copies a path saved with 'path'.",Color3.fromRGB(255,255,255))
-	debounce = false
-end
-
-function cmds.exec(...)
-	debounce = true
-	local code = {...}
-	local suc,err = pcall(function() loadstring(table.concat(code))() end)
-	if suc then
-		SendLog("Executed code.",Color3.fromRGB(0,180,0))
-	else
-		SendLog("Failed to execute code! Error: " .. err, Color3.fromRGB(200,0,0))
-	end
-	debounce = false
-end
-
-function cmds.path(mode,name,path)
-	debounce = true
-	if mode == "new" then
-		if not shortPaths[name] then
-			shortPaths[name] = path
-			SendLog("Saved path " .. path .. " under " .. name .. ".",Color3.fromRGB(0,180,0))
-		else
-			SendLog("Path already exists.",Color3.fromRGB(255,255,0))
-		end
-	elseif mode == "del" then
-		if shortPaths[name] then
-			shortPaths[name] = nil
-			SendLog("Removed path " .. path .. ".",Color3.fromRGB(0,180,0))
-		else
-			SendLog("Path does not exist.",Color3.fromRGB(255,255,0))
-		end
-	end
-	debounce = false
-end
-
-function cmds.copy_path(name)
-	debounce = true
-	if shortPaths[name] then
-		syn.write_clipboard(shortPaths[name])
-		SendLog("Copied path for " .. name .. ".",Color3.fromRGB(0,180,0))
-	else
-		SendLog("Path does not exist.",Color3.fromRGB(255,255,0))
-	end
-	debounce = false
-end
-
-local function HandleCommand()
-	local cmd = inputBox.Text
-	local args = splitString(cmd," ")
-	local invoke = args[1]
-	local nargs = {}
-	if #args > 1 then for i=2,#args do table.insert(nargs,#nargs+1,args[i]) end end
-	inputBox.Text = ""
-	if cmds[invoke] then 
-		if not debounce then 
-			SendLog("> " .. cmd,Color3.fromRGB(255,255,255))
-			cmds[invoke](unpack(nargs))
-		end
-	else
-		SendLog("Invalid invoke! Use cmds to view all the commands.",Color3.fromRGB(200,0,0))
-	end
-end
-
-services.UserInputService.InputEnded:Connect(function(key,gpe)
-	if gpe then return end
-	if key.KeyCode == Enum.KeyCode.P then
-		gui.Enabled = not gui.Enabled
-	end
-	if key.KeyCode == Enum.KeyCode.Semicolon then 
-		if not gui.Enabled then return end
-		inputBox:CaptureFocus()
-	end
-	if key.KeyCode == Enum.KeyCode.Return and focused then 
-		if not gui.Enabled then return end
-		HandleCommand()
-	end
-end)
-
-SendLog("Dev Console v1 by Argoz. Please keep in mind that this is in early alpha.",Color3.fromRGB(255,255,255))
-SendLog("Use cmds to view all the commands.",Color3.fromRGB(255,255,255))
-
-while wait(.1) do 
-	focused = inputBox:IsFocused()
-end
