20ec38ccd241502e9eeac1759229c9b7a1abdd9d
Ultrasonic1209 <44583181+Ultrasonic1209@users.noreply.github.com>
2019-07-27 21:19:12 +0100

    Update source.lua

--- docs/source.lua
+++ docs/source.lua
@@ -300,3 +300,10 @@ getgenv().fetchTable(tab,...)
         end
     end
 end
+
+getgenv().splitString = function(inputstr, sep)
+        if sep == nil then sep = "%s" end
+        local t={}
+        for str in string.gmatch(inputstr, "([^"..sep.."]+)") do table.insert(t, str) end
+        return t
+end
